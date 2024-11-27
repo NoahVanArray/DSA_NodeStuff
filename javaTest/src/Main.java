ff7225661bc74041f3f3a3c1cf99d558a764d345
yoe <128493923+NoahVanArray@users.noreply.github.com>
2024-11-28 00:34:10 +0800

    amogus skibidi not working thing that must be made to work

--- javaTest/src/Main.java
+++ javaTest/src/Main.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
+
+
 
 class BookNode{
 
@@ -16,12 +18,12 @@ class BookNode{
         "900 - 999 ~ History and geography"
     };
 
-    List<String> bookList = new ArrayList<>();
+    List<BookNode> bookList = new ArrayList<>();
     
-    private String title, author;
-    private int deweyClass, yearPublished;
+    public String title, author;
+    public int deweyClass, yearPublished;
     
-
+    BookNode(){}
     BookNode(String title, String author, int deweyClass, int yearPublished){
         this.title = title;
         this.author = author;
@@ -29,81 +31,112 @@ class BookNode{
         this.yearPublished = yearPublished;
     }
 
-    public void setNextNode(Node node){
-        this.next = node;
-    }
-    
-    public Node getNextNode(){
-        return this.next;
-    }
 }
 
 public class Main {
     public static boolean exitTheUser = false;
-
-
-    public void addBook(BookNode book){
-        Scanner bookScan = new Scanner(System.in);
-        String bookTitle, bookAuthor, bookClass, bookYear;
-        System.out.print("Enter title: ");
-        bookTitle = bookScan.nextLine();
-        System.out.print("Enter author name: ");
-        bookAuthor = bookScan.nextLine();
-        System.out.print("Enter Dewey Decimal class: ");
-        bookClass = bookScan.nextInt();
-        System.out.print("Enter year published: ");
-        bookYear = bookScan.nextInt();
-        
-
-        BookNode addedBook = new BookNode(bookTitle, bookAuthor, bookClass, bookYear0);
-        addedBook.bookList.Add(addedBook);
-        
-        bookScan.Close();
+    public static Scanner bookScan = new Scanner(System.in);
+    public static void viewBooks()
+    {
+        BookNode book = new BookNode();
+        forEach (BookNode bookItem : bookList) {
+            System.out.println(bookItem.book.title);
+            System.out.println(book.author);
+            System.out.println(book.deweyClass);
+            System.out.println(book.yearPublished);
+        }
     }
+    
+    public static void editBook()
+    {
+    
+    }   
 
+    public static void deleteBook()
+    {
+    
+    }  
+    
+    public static void addBook(){
+        String bookTitle = "", bookAuthor = "";
+        int bookClass = 0, bookYear = 0;
+        
+        System.out.print("Enter title: ");
+        if(bookScan.hasNextLine() ){
+            bookTitle = bookScan.nextLine();
+        }
+        else {bookTitle = "";}
+        
+        System.out.print("Enter author name: ");
+        if(bookScan.hasNextLine() ){
+            bookAuthor = bookScan.nextLine();
+        }
+        else {bookAuthor = "";}
+        
+        System.out.print("Enter Dewey Decimal class: ");
+        if(bookScan.hasNextInt() ){
+            bookClass = bookScan.nextInt();    
+        }
+        else {bookClass = 0;}
+        
+        System.out.print("Enter year published: ");
+        if(bookScan.hasNextInt() ){
+            bookYear = bookScan.nextInt();    
+        }
+        else {bookYear = 0;}
+        
+
+        BookNode addedBook = new BookNode(bookTitle, bookAuthor, bookClass, bookYear);
+        addedBook.bookList.add(addedBook);
+
+        System.out.println("\nBook added successfully.\n\n");
+    }
+    
     public static void userExit(boolean exit){
         if(exit)
         {
             exitTheUser = true;
         }
-}
-
+    }
+    
     public static void main(String[] args){
-        
-            System.out.println("Welcome to the Library.");
-            do{
-                Scanner scan = new Scanner(System.in);
-                System.out.println("\n1.) View Books \n2.) Add Book \n3.) Edit Book \n4.) Delete Book \n5.) Exit");
-                System.out.print("Your choice: ");
-                int choice = scan.nextInt();
-                switch (choice){
-                    case 1:
-                        //viewBooks();
-                        break;
-    
-                    case 2:
-                        //addBook();
-                        break;
-    
-                    case 3:
-                        //editBook();
-                        break;
+        int choice = 0;
+        System.out.println("Welcome to the Library.");
+        do{
+            Scanner scan = new Scanner(System.in);
+            System.out.println("What do you want to do: \n1.) View Books \n2.) Add Book \n3.) Edit Book \n4.) Delete Book \n5.) Exit");
+            System.out.print("Your choice: ");
+            if(scan.hasNextInt() ){
+                choice = scan.nextInt();    
+            }
+            switch (choice){
+                case 1:
+                viewBooks();
+                break;
+                
+                case 2:
+                addBook();
+                break;
 
-                    case 4:
-                        //deleteBook();
-                        break;
-                    
-                    case 5:
-                        userExit(true);
-                    break;
+                case 3:
+                editBook();
+                break;
+                case 4:
+                deleteBook();
+                break;
+            
+                case 5:
+                userExit(true);
+                break;
+
                 default:
                 System.out.print("Skill issue... try again.");
                 }
-                scan.close();
-            } while(exitTheUser == false);
+          
+        } while(exitTheUser == false);
+        bookScan.close();
         
-        
-        System.out.print("Bye bye...");
+    System.out.print("Bye bye...");
     }
 }
 
